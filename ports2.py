7dd2c4ea067957a36a3b180913a98b32d5d2e500
Michail Lelyakin <power_2007_pro@mail.ru>
2016-02-20 21:34:53 +0400

    Update ports2.py

--- ports2.py
+++ ports2.py
@@ -5,19 +5,25 @@ import serial
 import time
 from Tkinter import *
 import PIL
+import json
 
 root = Tk()
+f = open("conf.json", "r")
+json_data = json.loads(f.read())
+f.close()
 
 image_path = "spring.gif"
 text_file_path = 'C:/Users/Forward-TA/Desktop/time.txt'
 
-c = Canvas(root, width=600, height=400)
+c = Canvas(root, width=json_data["width"], height=json_data["height"])
 c.pack()
 
 img = PhotoImage(file=image_path)
 #c.create_image(0, 0, image=img, anchor="nw")
-c.create_rectangle(0,0,600,400,fill="black",outline="blue")
-c.create_text(20,30,text="kefal", font="Verdana 12",anchor="w",justify=CENTER,fill="red") 
+
+
+
+c.create_rectangle(0,0,json_data["width"],json_data["height"],fill=json_data["color"],outline="blue")
 
 def function(ar="kefal"):
     with serial.Serial("COM3", 19200, timeout=1) as ser:
@@ -52,17 +58,26 @@ def function(ar="kefal"):
                     score2 = data[5]
         
                     minuts_and_seconds = str(minuts) + ":" + str(second)
-                    attach = str(attach)
+                    if attach !=255:
+                        if attach > 24:
+                            attach = (attach & 127) / 10.0
+
+                        attach = str(attach)
+
+                    else:
+                        attach = ""
                     score1 = str(score1)
                     score2 = str(score2)
         
                     #c.create_image(0, 0, image=img, anchor="nw")
-                    c.create_rectangle(0,0,600,400,fill="black",outline="blue")
-                    c.create_text(20,30,text=minuts_and_seconds, 
-                        font="Verdana 12",anchor="w",justify=CENTER,fill="red") 
+                    c.create_rectangle(0,0,json_data["width"],json_data["height"],
+                        fill=json_data["color"],outline="blue")
+
+                    c.create_text(json_data["position"][0],json_data["position"][1],text=attach, 
+                        font=json_data["font"],anchor="w",justify=CENTER,fill=json_data["colorFill"]) 
         
-                    c.create_text(20,60,text=score1 + ":" + score2, 
-                        font="Verdana 12",anchor="w",justify=CENTER,fill="red") 
+                    #c.create_text(20,60,text=score1 + ":" + score2, 
+                    #    font="Verdana 12",anchor="w",justify=CENTER,fill="red") 
         except:
             pass
     root.after(1, function)
